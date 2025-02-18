b4697f09cde24e978d8eb0394988407212db4bb6
Amar <amarrathore045@gmail.com>
2025-02-19 00:09:06 +0530

    Added boiler-plate code

---
+++ src/main/java/com/org/picbucket/PicBucketApplication.java
@@ -0,0 +1,13 @@
+package com.org.picbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicBucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/picbucket/PicBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.picbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
