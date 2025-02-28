a4bdbc2bc1bd53dc5e4dfeccd465f0206e12f3aa
Amar <amarrathore045@gmail.com>
2025-02-28 13:22:08 +0530

    Added html file for about and book to be modified later

--- src/main/java/com/org/picbucket/controller/HomeController.java
+++ src/main/java/com/org/picbucket/controller/HomeController.java
@@ -11,4 +11,14 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/about")
+    public String about(Model model){
+        return "about";
+    }
+
+    @GetMapping("/book")
+    public String book(Model model){
+        return "book";
+    }
+
 }
